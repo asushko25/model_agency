8cee2be13bbf0ce757840923c7b0f58b9a30df36
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2025-01-14 12:49:46 +0200

    feat(models.py): implement custom User model with phone number and fields

--- backend/model/models.py
+++ backend/model/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        extra_fields.setdefault("first_name", "Admin")
+        extra_fields.setdefault("last_name", "User")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    phone_number = PhoneNumberField(max_length=15, blank=True, null=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
+    class Meta:
+        ordering = ["-id"]
+        verbose_name = "User"
+        verbose_name_plural = "Users"
