bb0e77e96c4240f1496253269d92c58f1031ced3
OlehPro <olehoryshshuk@gmail.com>
2025-01-21 20:46:21 +0100

    refact: Change admin site and forms.py. Because of changes in User model

--- backend/model/forms.py
+++ backend/model/forms.py
@@ -5,7 +5,7 @@ from .models import User
 class UserCreationForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ("email", "first_name", "last_name", "phone_number")
+        fields = ("email", "full_name", "phone_number")
 
     def clean_password(self):
         return None
