519005607c425747875dd467ec1c7690c24f071f
OlehPro <olehoryshshuk@gmail.com>
2025-01-22 12:12:40 +0100

    perf(model_agency/settings/): Configure Django settings during Development and Production

---
+++ backend/model_agency/settings/prod.py
@@ -0,0 +1,37 @@
+"""Django configurations during Production"""
+
+import os
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = ["www.example.com"]
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+    }
+}
+
+# Email configurations
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
