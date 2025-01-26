a87879254a4ca22d13a4c9be34260187fc06b89d
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2025-01-26 09:44:57 +0200

    feat(contact/admin.py): register Contact model in admin panel

--- backend/contact/admin.py
+++ backend/contact/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact.models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    search_fields = ("name", "last_name", "email")
+    list_display = (
+        "full_name",
+        "email",
+        "phone_number",
+        "model",
+        "message",
+        "created_at",
+    )
+
+    def full_name(self, obj):
+        return obj.full_name
+
+
+admin.site.register(Contact, ContactAdmin)
