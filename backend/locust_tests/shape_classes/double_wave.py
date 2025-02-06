1d9fc935020b37d2da2948772ecc51d8e1bc8259
OlehPro <olehoryshshuk@gmail.com>
2025-02-06 16:20:47 +0100

    feat(locust_tests/shape_classes): Create 2 modules, Custom Load Test Shapes

---
+++ backend/locust_tests/shape_classes/double_wave.py
@@ -0,0 +1,40 @@
+import math
+from locust import LoadTestShape
+
+
+class DoubleWave(LoadTestShape):
+    """
+    A shape to imitate some specific user behaviour. In this example, midday
+    and evening meal times. First peak of users appear at time_limit/3 and
+    second peak appears at 2*time_limit/3
+
+    Also you can pass below arguments using CLI. Exp:
+    `locust --min-users 10 --peak-one-users 50 --peak-two-users 100 --time-limit 300 \
+    locustfile,locust_tests/shape_classes/double_wave.py`
+
+    Settings:
+        min_users -- minimum users
+        peak_one_users -- users in first peak
+        peak_two_users -- users in second peak
+        time_limit -- total length of test
+    """
+
+    min_users = 20
+    peak_one_users = 80
+    peak_two_users = 120
+    time_limit = 600
+
+    def tick(self):
+        run_time = round(self.get_run_time())
+
+        if run_time < self.time_limit:
+            user_count = (
+                (self.peak_one_users - self.min_users)
+                * math.e ** -(((run_time / (self.time_limit / 10 * 2 / 3)) - 5) ** 2)
+                + (self.peak_two_users - self.min_users)
+                * math.e ** -(((run_time / (self.time_limit / 10 * 2 / 3)) - 10) ** 2)
+                + self.min_users
+            )
+            return round(user_count), round(user_count)
+        else:
+            return None
